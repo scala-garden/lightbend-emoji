3f39d539d4e17e2c77f76a60f89c3e039cf2a60b
Seth Tisue <seth@tisue.net>
2026-07-31 14:10:09 -0700

    clean up unnecessary code after 2.12 removal

--- src/main/scala-3/com/lightbend/emoji/ScalaVersionSpecific.scala
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (C) Lightbend Inc. <http://www.lightbend.com>
- */
-package com.lightbend.emoji
-
-object ScalaVersionSpecific:
-  def checkLengths(sc: StringContext, args: Seq[Any]): Unit =
-    StringContext.checkLengths(args, sc.parts)
